6a0e1844e3963c14cabe48da89f7035ecfac0649
CezaryZal <czarek0821@gmail.com>
2020-05-05 00:12:22 +0200

    Create validate for Meeting in MeetingService class
    Create MeetingServiceTest class;
    Update MeetingService class;
    Create InvalidCalendarListException class;

--- src/main/java/com/orange/task/service/MeetingService.java
+++ src/main/java/com/orange/task/service/MeetingService.java
@@ -2,10 +2,12 @@ package com.orange.task.service;
 
 import com.orange.task.model.Calendar;
 import com.orange.task.model.Meeting;
+import com.orange.task.validations.MeetingValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalTime;
+import java.util.Collection;
 import java.util.Set;
 
 @Service
@@ -18,11 +20,28 @@ public class MeetingService {
         this.meetingCalculator = meetingCalculator;
     }
 
-    public Set<Meeting> getMeetingSuggestions(Set<Calendar> calendarList, LocalTime inputDuration){
+    public Set<Meeting> getMeetingSuggestions(Set<Calendar> calendarSet, LocalTime inputDuration){
+        validateMeetingInInputCalendarSet(calendarSet);
+        
         return meetingCalculator.calculateAvailableMeeting(
-                calendarList,
+                calendarSet,
                 inputDuration);
-        //początkowo format daty przy deserializacji, potem do obsługi błedów własny, wewnątrzny
-//        LocalTime.parse(inputDuration, dateTimeFormatter));
+    }
+
+    private void validateMeetingInInputCalendarSet(Set<Calendar> calendarSet){
+        calendarSet.stream()
+                .map(Calendar::getPlannedMeeting)
+                .flatMap(Collection::stream)
+                .forEach(inputMeeting -> {
+                    MeetingValidator.validateMeetingFromValidationConstraints(inputMeeting);
+                    MeetingValidator.validateStartAndEndTimes(inputMeeting);
+                });
+
+        calendarSet.stream()
+                .map(Calendar::getWorkingHours)
+                .forEach(inputMeeting -> {
+                    MeetingValidator.validateMeetingFromValidationConstraints(inputMeeting);
+                    MeetingValidator.validateStartAndEndTimes(inputMeeting);
+                });
     }
 }
---
+++ src/test/java/com/orange/task/service/MeetingServiceTest.java
@@ -0,0 +1,153 @@
+package com.orange.task.service;
+
+import com.google.common.collect.Sets;
+import com.orange.task.exceptions.invalid.InvalidMeetingException;
+import com.orange.task.model.Calendar;
+import com.orange.task.model.Meeting;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalTime;
+import java.util.HashSet;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class MeetingServiceTest {
+
+    @Spy
+    private WorkingTimeTogetherGenerator workingTimeTogetherGenerator;
+
+    @InjectMocks
+    private MeetingCalculator meetingCalculator;
+
+    private MeetingService meetingService;
+    private HashSet<Calendar> sampleCollectionOfCalendar;
+
+    @BeforeEach
+    void setUp() {
+        meetingService = new MeetingService(meetingCalculator);
+//        sampleCollectionOfCalendar = Sets.newHashSet(
+//                new Calendar(new Meeting(LocalTime.of(9, 30), LocalTime.of(16, 0)),
+//                        Sets.newHashSet(new Meeting(LocalTime.of(9, 30), LocalTime.of(10, 0)),
+//                                new Meeting(LocalTime.of(11, 30), LocalTime.of(13, 30)),
+//                                new Meeting(LocalTime.of(15, 30), LocalTime.of(16, 0)))),
+//                new Calendar(new Meeting(LocalTime.of(8, 0), LocalTime.of(18, 0)),
+//                        Sets.newHashSet(new Meeting(LocalTime.of(9, 0), LocalTime.of(11, 0)),
+//                                new Meeting(LocalTime.of(12, 30), LocalTime.of(13, 0)),
+//                                new Meeting(LocalTime.of(14, 30), LocalTime.of(16, 0)),
+//                                new Meeting(LocalTime.of(16, 0), LocalTime.of(17, 30)))
+//                ));
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenSendingNullToStartTimeToPlannedMeeting(){
+        String expectedMessage = "The 'start' should not be null";
+        sampleCollectionOfCalendar = Sets.newHashSet(
+                new Calendar(new Meeting(LocalTime.of(9, 30), LocalTime.of(16, 0)),
+                        Sets.newHashSet(new Meeting(LocalTime.of(9, 30), LocalTime.of(10, 0)),
+                                new Meeting(LocalTime.of(11, 30), LocalTime.of(13, 30)),
+                                new Meeting(null, LocalTime.of(15, 30)))
+                ));
+
+        Exception exception = assertThrows(InvalidMeetingException.class, () -> {
+            meetingService.getMeetingSuggestions(sampleCollectionOfCalendar, LocalTime.of(1,2));
+        });
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenSendingNullToEndTimeToPlannedMeeting(){
+        String expectedMessage = "The 'end' should not be null";
+        sampleCollectionOfCalendar = Sets.newHashSet(
+                new Calendar(new Meeting(LocalTime.of(9, 30), LocalTime.of(16, 0)),
+                        Sets.newHashSet(new Meeting(LocalTime.of(9, 30), LocalTime.of(10, 0)),
+                                new Meeting(LocalTime.of(11, 30), LocalTime.of(13, 30)),
+                                new Meeting(LocalTime.of(15, 30), null))
+                ));
+
+        Exception exception = assertThrows(InvalidMeetingException.class, () -> {
+            meetingService.getMeetingSuggestions(sampleCollectionOfCalendar, LocalTime.of(1,2));
+        });
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenSendingEndTimeIsBeforeStartTimeInPlannedMeeting(){
+        String expectedMessage = "The end time of Meeting is before start time.";
+        sampleCollectionOfCalendar = Sets.newHashSet(
+                new Calendar(new Meeting(LocalTime.of(9, 30), LocalTime.of(16, 0)),
+                        Sets.newHashSet(new Meeting(LocalTime.of(9, 30), LocalTime.of(10, 0)),
+                                new Meeting(LocalTime.of(11, 30), LocalTime.of(13, 30)),
+                                new Meeting(LocalTime.of(15, 30), LocalTime.of(11, 30)))
+                ));
+
+        Exception exception = assertThrows(InvalidMeetingException.class, () -> {
+            meetingService.getMeetingSuggestions(sampleCollectionOfCalendar, LocalTime.of(1,2));
+        });
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenSendingNullToStartTimeToWorkingHours(){
+        String expectedMessage = "The 'start' should not be null";
+        sampleCollectionOfCalendar = Sets.newHashSet(
+                new Calendar(new Meeting(null, LocalTime.of(16, 0)),
+                        Sets.newHashSet(new Meeting(LocalTime.of(9, 30), LocalTime.of(10, 0)),
+                                new Meeting(LocalTime.of(11, 30), LocalTime.of(13, 30)),
+                                new Meeting(LocalTime.of(14, 30), LocalTime.of(15, 30)))
+                ));
+
+        Exception exception = assertThrows(InvalidMeetingException.class, () -> {
+            meetingService.getMeetingSuggestions(sampleCollectionOfCalendar, LocalTime.of(1,2));
+        });
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenSendingNullToEndTimeToWorkingHours(){
+        String expectedMessage = "The 'end' should not be null";
+        sampleCollectionOfCalendar = Sets.newHashSet(
+                new Calendar(new Meeting(LocalTime.of(9, 30), null),
+                        Sets.newHashSet(new Meeting(LocalTime.of(9, 30), LocalTime.of(10, 0)),
+                                new Meeting(LocalTime.of(11, 30), LocalTime.of(13, 30)),
+                                new Meeting(LocalTime.of(15, 30), LocalTime.of(16, 30)))
+                ));
+
+        Exception exception = assertThrows(InvalidMeetingException.class, () -> {
+            meetingService.getMeetingSuggestions(sampleCollectionOfCalendar, LocalTime.of(1,2));
+        });
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenSendingEndTimeIsBeforeStartTimeInWorkingHours(){
+        String expectedMessage = "The end time of Meeting is before start time.";
+        sampleCollectionOfCalendar = Sets.newHashSet(
+                new Calendar(new Meeting(LocalTime.of(16, 30), LocalTime.of(16, 0)),
+                        Sets.newHashSet(new Meeting(LocalTime.of(9, 30), LocalTime.of(10, 0)))
+                ));
+
+        Exception exception = assertThrows(InvalidMeetingException.class, () -> {
+            meetingService.getMeetingSuggestions(sampleCollectionOfCalendar, LocalTime.of(1,2));
+        });
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+}
